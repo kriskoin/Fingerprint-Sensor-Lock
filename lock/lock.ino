4668c6cced3a239e52541529b8c9036bdae8abc2
KrisKoin <kriskoin@localhost.localdomain>
2019-08-10 12:53:10 -0600

    wip: more led color status

--- lock/lock.ino
+++ lock/lock.ino
@@ -2,7 +2,23 @@
 const int LED_PIN_RED = 9;
 const int LED_PIN_GREEN = 10;
 const int LED_PIN_BLUE = 11;
-const int DISPLAY_TIME = 100; //millisconds
+const int DISPLAY_TIME = 200; //milliseconds
+
+//led status
+enum led_status {
+  READING,
+  ENROLL,
+  OK,
+  ERR,
+  NONE
+};
+
+//lock status
+enum lock_status {
+  LOCK,
+  UNLOCK
+};
+
 
 /*
  * Setup the rgb values for the led
@@ -14,10 +30,23 @@ void set_led(int r,int g, int b,int delay_time){
   delay(delay_time);
 }
 
+void led_print_red(){
+  set_led(255,0,0,DISPLAY_TIME);
+}
+void led_print_orange(){
+  set_led(200,29,0,DISPLAY_TIME);
+}
+void led_print_green(){
+  set_led(0,255,0,DISPLAY_TIME);
+}
 void led_print_blue(){
-  Serial.println("print blue");
   set_led(0,0,255,DISPLAY_TIME);
 }
+
+void led_print_clear(){
+  set_led(0,0,0,DISPLAY_TIME);
+}
+
 
 void setup() {
  
@@ -25,6 +54,16 @@ void setup() {
 }
 
 void loop() {
+  /*
+  led_print_red();
+  delay(1000);
+  led_print_green();
+  delay(1000);
   led_print_blue();
-
+  delay(1000);*/
+  led_print_orange();
+  delay(1000);
+  /*
+  led_print_clear();
+  delay(1000);*/
 }
